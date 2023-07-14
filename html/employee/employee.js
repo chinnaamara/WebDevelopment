9e8cb9ee145e7d7eb42334932e1e1c362f82d588
chinnaamara <amara.chinna@gmail.com>
2023-07-14 22:31:59 +0530

    Added test js file

---
+++ html/employee/employee.js
@@ -0,0 +1,16 @@
+var save = function() {
+	alert('saving employee data of ' + document.getElementById('empName').value);
+}
+
+// var name = 'Arjun';  // variable declaration
+// var	name = 'jgjh';
+if (true) {
+	let name = "Sai";
+	let Name = 'Ram';
+	console.log('Name inside- 1:', name);
+	console.log('Name inside - 2:', Name);
+}
+
+console.log('Name outside:', name);
+
+
